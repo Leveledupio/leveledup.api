e2a3c8b77d39a2c85be4692daffaa60e0cf80731
James Strong <strong.james.e@gmail.com>
2017-08-17 20:33:09 -0400

    update handler

--- handlers/contact_handler.go
+++ handlers/contact_handler.go
@@ -4,8 +4,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"errors"
-	"github.com/strongjz/leveledup-api/models"
 	"github.com/gin-gonic/gin"
+	"github.com/strongjz/leveledup-api/models"
 	"net/http"
 )
 
@@ -15,13 +15,15 @@ func (h *ApiResource) Contact(c *gin.Context) {
 
 	email := models.NewEmail(h.DB, h.AWSSession)
 
-
 	err := c.Bind(&email.EmailRow)
 	if err != nil {
 		log.Errorf("Problem decoding JSON body %s", err)
 		c.JSON(400, errors.New("problem decoding body"))
 		return
 	}
+
+	email.EmailTo = "clientservices@leveledup.io"
+	email.Subject = "Contact Us Form"
 
 	log.Debugf("Contact Handler Email Subject: %s", email.Subject)
 	log.Debugf("Contact Handler Email to: %s", email.EmailTo)
